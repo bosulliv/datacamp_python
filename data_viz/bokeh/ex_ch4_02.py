7f6a57c03e2f95952e87ea545086b0d33e24abe8
Brian Osullivan <brian.osullivan@8x8.com>
2019-02-03 17:51:47 +0000

    learn: finish chapter4, and the course

---
+++ data_viz/bokeh/ex_ch4_02.py
@@ -0,0 +1,51 @@
+# Data Prep
+import pandas as pd
+file = 'gapminder_tidy.csv'
+data = pd.read_csv(file, index_col='Year')
+
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
+from bokeh.io import curdoc
+
+source = ColumnDataSource(data={
+    'x'       : data.loc[1970].fertility,
+    'y'       : data.loc[1970].life,
+    'country'      : data.loc[1970].Country,
+    'pop'      : (data.loc[1970].population / 20000000) + 2,
+    'region'      : data.loc[1970].region,
+})
+
+# Save the minimum and maximum values of the fertility column: xmin, xmax
+xmin, xmax = min(data.fertility), max(data.fertility)
+
+# Save the minimum and maximum values of the life expectancy column: ymin, ymax
+ymin, ymax = min(data.life), max(data.life)
+
+# Create the figure: plot
+plot = figure(title='Gapminder Data for 1970', plot_height=400, plot_width=700,
+              x_range=(xmin, xmax), y_range=(ymin, ymax))
+
+#--- Datacamp exercise code below 
+
+# Make a list of the unique values from the region column: regions_list
+regions_list = data.region.unique().tolist()
+
+# Import CategoricalColorMapper from bokeh.models and the Spectral6 palette from bokeh.palettes
+from bokeh.models import CategoricalColorMapper
+from bokeh.palettes import Spectral6
+
+# Make a color mapper: color_mapper
+color_mapper = CategoricalColorMapper(factors=regions_list, palette=Spectral6)
+
+# Add the color mapper to the circle glyph
+plot.circle(x='x', y='y', fill_alpha=0.8, source=source,
+            color=dict(field='region', transform=color_mapper), legend='region')
+
+# Set the legend.location attribute of the plot to 'top_right'
+plot.legend.location = 'top_right'
+
+# Add the plot to the current document and add the title
+curdoc().add_root(plot)
+curdoc().title = 'Gapminder'
+
+# bokeh serve --show ex_ch4_02.py
