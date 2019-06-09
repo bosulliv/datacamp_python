d0a2b6dc018d7f8e2e7a317764abe7026085d792
Brian Osullivan <brian.osullivan@8x8.com>
2019-06-09 11:35:12 +0100

    learn: finish chapter 3

--- sql/joining_data/ch2-psql.sql
+++ sql/joining_data/ch2-psql.sql
@@ -12,3 +12,28 @@ create table currencies(
 -- import data
 \copy currencies from 'currencies.csv' DELIMITER ',' CSV HEADER;
 
+
+-- create table economies2010
+drop table IF EXISTS economies2010;
+create table economies2010(
+    "code" text primary key,
+    "year" int,
+    "income_group" text,
+    "gross_savings" float
+);
+
+-- import economies
+\copy economies2010 from 'economies2010.csv' DELIMITER ',' CSV HEADER;
+
+-- create table economies2015
+drop table IF EXISTS economies2015;
+create table economies2015(
+    "code" text primary key,
+    "year" int,
+    "income_group" text,
+    "gross_savings" float
+);
+
+-- import economies
+\copy economies2015 from 'economies2015.csv' DELIMITER ',' CSV HEADER;
+
